90a3676000f44263d28816ccca534125c35ee02a
gilberto <levin@readytheory.com>
2024-02-19 09:16:57 -0600

    correct path to run manage.py

--- django_site/django_root/settings.py
+++ django_site/django_root/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
--- render-build.sh
+++ render-build.sh
@@ -6,7 +6,7 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-cd ./settings
+cd ./django_root
 # Convert static asset files
 python manage.py collectstatic --no-input
 
