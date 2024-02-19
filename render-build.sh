a473ceda9aef0567077b8f51eab8b0d1242aff13
gilberto <levin@readytheory.com>
2024-02-19 10:20:44 -0600

    change to django_site

--- render-build.sh
+++ render-build.sh
@@ -6,7 +6,7 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-cd ./django_root
+cd ./django_site
 # Convert static asset files
 python manage.py collectstatic --no-input
 
