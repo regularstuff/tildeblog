e9a662fd3b28762747ea67709e3142e0ee579cad
gilberto <levintennine@gmail.com>
2024-03-03 21:54:48 -0600

    Create page to show content when use clicks headline

--- django_site/til/urls.py
+++ django_site/til/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("de", views.learning_data_entry, name="til_de"),
+    path("show/<int:learnt_id>", views.show, name="til_show"),
     path("", views.landing_page, name="til_landing"),
 ]
--- django_site/til/views.py
+++ django_site/til/views.py
@@ -6,11 +6,28 @@ from .forms import LearningForm
 from .models import Learned
 
 
+def show(request, learnt_id):
+    learnt = Learned.objects.get(id=learnt_id)
+    body = learnt.content
+    title = learnt.title
+    page_title = "placeholder123"
+    tldr = learnt.tldr
+    tags = learnt.tags
+    context = {
+        "learn_title": title,
+        "learnt_content": body,
+        "learnt_tldr": tldr,
+        "learnt_tags": tags,
+        "page_title": page_title,
+    }
+
+    return render(request, template_name="til/display_learnt.html", context=context)
+
+
 def landing_page(request):
     mylearnings = Learned.objects.all()
     learnt_things = [
-        {"headline": x.title, "url": f"https://x/{x.id}", "tags": [1, 2, 3]}
-        for x in mylearnings
+        {"headline": x.title, "id": x.id, "tags": [1, 2, 3]} for x in mylearnings
     ]
 
     return render(
@@ -29,5 +46,5 @@ def learning_data_entry(request):
         learning_form = LearningForm(request.POST)
         if learning_form.is_valid():
             learning_form.save()
-            return render(request, 'til/post_thanks.html')
+            return render(request, "til/post_thanks.html")
     return HttpResponseRedirect("")
