d53f453feb5314f0eb6d7366ff2c1bef30507433
gilberto <levintennine@gmail.com>
2024-03-04 16:59:42 -0600

    run createsuperuser
    see https://community.render.com/t/is-it-possible-to-create-django-admin-account-with-free-plan/6972/2

--- render-build.sh
+++ render-build.sh
@@ -2,8 +2,6 @@
 # Exit on error -- this is for render
 set -o errexit
 
-
-
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 cd ./django_site
@@ -13,3 +11,9 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
+# to control this you set environment variable in render control panel
+
+if [[ "$CREATE_SUPERUSER"=YES ]] ;
+then
+  python manage.py createsuperuser --no-input
+fi
