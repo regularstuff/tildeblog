efbbc357e1174435cd6863e1bf5533dc93a0e38e
Nair, Anuj <anujnairr@gmail.com>
2024-03-09 00:15:13 +0530

    Add an Edit button on the learnt-item view page #20

--- django_site/til/urls.py
+++ django_site/til/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("de/<int:learnt_id>", views.learning_data_entry, name="til_de_id"),
     path("de", views.learning_data_entry, name="til_de"),
     path("show/<int:learnt_id>", views.show, name="til_show"),
     path("", views.landing_page, name="til_landing"),
--- django_site/til/views.py
+++ django_site/til/views.py
@@ -13,8 +13,10 @@ def show(request, learnt_id):
     page_title = "placeholder123"
     tldr = learnt.tldr
     tags = learnt.tags
+    id = learnt.id
     context = {
-        "learn_title": title,
+        "learnt_id": learnt_id,
+        "learnt_title": title,
         "learnt_content": body,
         "learnt_tldr": tldr,
         "learnt_tags": tags,
@@ -33,17 +35,22 @@ def landing_page(request):
     return render(
         request,
         "til/landing.html",
-        context={"title": "ABC: Always Be Learning", "learnings": learnt_things},
+        context={"title": "ABC: Always Be Learning",
+                 "learnings": learnt_things},
     )
 
 
-def learning_data_entry(request):
+def learning_data_entry(request, learnt_id=None):
+    learnt = None
+    if learnt_id != None:
+        learnt = Learned.objects.get(id=learnt_id)
     if request.method == "GET":
         context = {"page_title": "Learning Data Entry Form"}
-        context["learning_form"] = LearningForm()
+        context["learning_form"] = LearningForm(instance=learnt)
         return render(request, template_name="til/learning.html", context=context)
+
     if request.method == "POST":
-        learning_form = LearningForm(request.POST)
+        learning_form = LearningForm(request.POST, instance=learnt)
         if learning_form.is_valid():
             learning_form.save()
             return render(request, "til/post_thanks.html")
