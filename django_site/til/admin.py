b3081c3efb26a69d142afd5bc9c93eab58badd29
failedreality <jsweber@gmail.com>
2024-03-08 11:47:54 -0600

    Adding in more display options and searchable title field

--- django_site/til/admin.py
+++ django_site/til/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Learned, LearningTag
 
-# this will add the post link on the admin page
-admin.site.register(Learned)
+
+class LearnedAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['title', 'tldr', 'created']
+
+# this will add view and edit functionality on the admin page
+admin.site.register(Learned, LearnedAdmin)
 admin.site.register(LearningTag)
